1ac4f0298dd172d03ddfe88188c41209a195d392
lucasnsaraujo <lucasnsaraujo0310@gmail.com>
2022-06-28 00:48:27 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+import styles from '../styles/home.module.scss'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ln.ews</title>
+      </Head>
+      <div>
+        <h1>
+
+          Hello World 
+          
+        </h1>
+      </div>
+    </>
+  )
+}
