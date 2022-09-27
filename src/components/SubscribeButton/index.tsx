eb5cd7db43620c4a15552c2c2cf4e4f5537755cc
Lucas Araujo <lucas.araujo@codenapp.com>
2022-09-27 10:12:16 -0300

    saving update

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,26 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const {data: session, status} = useSession();
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+        try {
+            const response = await api.post('/subscribe')
+            const { sessionId } = response.data
+        } catch {
+            return;
+        }
+    }
     return (
         <button type="button" className={styles.subscribeButton}>
             Subscribe now
