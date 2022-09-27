5ff053748b7d8dfa526e150164c47d94116b8782
Lucas Nascimento <lucas.araujo@codenapp.com>
2022-09-27 18:03:49 -0300

    finished posts listing

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,50 +1,66 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import styles from './styles.module.scss'
-import { client } from '../../services/prismic';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import styles from "./styles.module.scss";
+import { client } from "../../services/prismic";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
-    return (
-        <>
-            <Head>
-                <title>Posts | Ignews</title>
-            </Head>
-            <main className={styles.container}>
-                <div className={styles.posts}>
-                    <a>
-                        <time>27 de setembro de 2022</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer. Enim sed faucibus turpis in eu mi. Diam sit amet nisl suscipit adipiscing bibendum. Purus in massa tempor nec feugiat.</p>
-                    </a>
-                    <a>
-                        <time>27 de setembro de 2022</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer. Enim sed faucibus turpis in eu mi. Diam sit amet nisl suscipit adipiscing bibendum. Purus in massa tempor nec feugiat.</p>
-                    </a>
-                    <a>
-                        <time>27 de setembro de 2022</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer. Enim sed faucibus turpis in eu mi. Diam sit amet nisl suscipit adipiscing bibendum. Purus in massa tempor nec feugiat.</p>
-                    </a>
-                    <a>
-                        <time>27 de setembro de 2022</time>
-                        <strong>Boas práticas para devs em início de carreira</strong>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer. Enim sed faucibus turpis in eu mi. Diam sit amet nisl suscipit adipiscing bibendum. Purus in massa tempor nec feugiat.</p>
-                    </a>
-                </div>
-            </main>
-        </>
-    )
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <a key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
 
-export const getStaticProps: GetStaticProps = async ({previewData}) => {
-    const page = await client.getAllByType('post')
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  const response = await client.getAllByType("post");
 
-    console.log(page)
+  console.log(response);
 
-    return {    
-        props: {
-            page
+  const posts = response.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
         }
-    }
-}
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
